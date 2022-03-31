d5efb670c46ebf24f3ac44a5a19cb2a8400941fd
Jonathan Arreguit <jarreguit@gmail.com>
2022-03-31 11:46:26 +0200

    [LAB5] Release

---
+++ Lab5/Python/lab5.py
@@ -0,0 +1,16 @@
+""" Lab 5 """
+from exercise1 import exercise1
+import farms_pylog as pylog
+
+
+def main():
+    """Main function that runs all the exercises."""
+    pylog.info('Implementing Lab 5 : Exercise 1')
+    exercise1()
+
+
+if __name__ == '__main__':
+    from cmcpack import parse_args
+    parse_args()
+    main()
+
